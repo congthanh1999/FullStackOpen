a3b8aef79fd15702fc9e8d809237d34be686c4a4
congthanh1999 <congthanh10021999@gmail.com>
2024-04-15 11:09:57 +0700

    create part4

--- part2/the-phonebook/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ searchInput, onChange }) => {
-  return (
-    <div className="filter">
-      filter shown with <input value={searchInput} onChange={onChange} />
-    </div>
-  );
-};
-
-export default Filter;
