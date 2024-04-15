3ae51399911d600c253a6b026cbf37204dc74ade
congthanh1999 <congthanh10021999@gmail.com>
2024-04-15 14:06:17 +0700

    blog-list step 2

---
+++ part4/blog-list/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+const config = require("./utils/config");
+
+const app = express();
+
+mongoose.connect(config.MONGODB_URI);
+
+app.use("/api/blogs", blogsRouter);
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,36 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const app = require("./app");
 
-const app = express();
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-const url =
-  "mongodb+srv://congthanh10021999:truongcongT99@blog-cluster.lgptlw3.mongodb.net/blog?retryWrites=true&w=majority&appName=blog-cluster";
-
-mongoose.connect(url);
-
-app.use(cors);
-app.use(express.json());
-
-app.get("/api/blogs", (req, res) => {
-  Blog.find({}).then((blogs) => res.json(blogs));
-});
-
-app.post("/api/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog.save.then((createdBlog) => res.status(204).json(createdBlog));
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on PORT ${config.PORT}`);
 });
