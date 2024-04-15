7a7424e01edf279392ae5472f1480a620d5f1e52
congthanh1999 <congthanh10021999@gmail.com>
2024-04-15 11:45:50 +0700

    copied project from part3

---
+++ part4/backend/index.js
@@ -0,0 +1,114 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const Person = require("./models/person");
+
+const app = express();
+
+morgan.token("body", function getBody(req) {
+  if (req.method === "POST") {
+    return JSON.stringify({ ...req.body });
+  }
+});
+
+app.use(express.static("dist"));
+app.use(cors());
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+app.get("/api/persons", (req, res) => {
+  Person.find({}).then((persons) => res.json(persons));
+});
+
+app.get("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+
+  Person.findByIdAndDelete(id).then(() => res.status(204).end());
+});
+
+app.post("/api/persons", (req, res, next) => {
+  const body = req.body;
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((updatedPerson) => res.json(updatedPerson))
+    .catch((error) => {
+      next(error);
+    });
+});
+
+app.get("/info", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      const current = new Date();
+      const content = `<div>Phonebook has info for ${persons.length} people</div>
+                      <br/>
+                      <div>${current}</div>`;
+      res.send(content);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "Malformatted" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
---
+++ part4/frontend/src/App.jsx
@@ -0,0 +1,131 @@
+import { useEffect, useState, useRef } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import personService from "./services/persons";
+import Notification from "./components/Notification";
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageColor, setMessageColor] = useState("");
+  const timerRef = useRef();
+
+  useEffect(() => {
+    personService.getAll().then((initialPersons) => setPersons(initialPersons));
+  }, []);
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const displayNotification = (message, color) => {
+    clearTimeout(timerRef.current);
+
+    setMessage(message);
+    setMessageColor(color);
+
+    timerRef.current = setTimeout(() => {
+      setMessage(null);
+    }, 5000);
+  };
+
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+
+    const newPerson = { name: newName, number: newNumber };
+
+    const addedPerson = persons.find(
+      (person) => person.name === newPerson.name
+    );
+    const updateAddedPerson = { ...addedPerson, number: newNumber };
+
+    if (addedPerson) {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        personService
+          .update(updateAddedPerson, updateAddedPerson.id)
+          // eslint-disable-next-line no-unused-vars
+          .then((updatedPerson) => {
+            if (updatedPerson) {
+              setPersons(
+                persons.map((person) =>
+                  person.name === newName ? updatedPerson : person
+                )
+              );
+            }
+
+            displayNotification(
+              `Updated ${updatedPerson.name}'s information`,
+              "hsl(120, 100%, 27%)"
+            );
+
+            setNewName("");
+            setNewNumber("");
+          })
+          // eslint-disable-next-line no-unused-vars
+          .catch((error) => {
+            const errorMessage =
+              newNumber && error.name === "TypeError"
+                ? `Information of ${newName} has already been removed from server`
+                : error.response.data.error;
+            displayNotification(errorMessage, `hsl(0, 100%, 40%)`);
+          });
+      }
+    } else {
+      personService
+        .create(newPerson)
+        .then((createdPerson) => {
+          setPersons([...persons, createdPerson]);
+          displayNotification(
+            `Added ${newPerson.name} to phonebook`,
+            "hsl(120, 100%, 27%)"
+          );
+          setNewName("");
+          setNewNumber("");
+        })
+        .catch((error) =>
+          displayNotification(error.response.data.error, `hsl(0, 100%, 40%)`)
+        );
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={message} color={messageColor} />
+      <Filter searchInput={searchInput} onChange={handleSearchInputChange} />
+      <h3>add a new</h3>
+      <PersonForm
+        onSubmitPerson={handleAddPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons
+        persons={persons}
+        searchInput={searchInput}
+        setPersons={setPersons}
+        displayNotification={displayNotification}
+      />
+    </div>
+  );
+};
+
+export default App;
