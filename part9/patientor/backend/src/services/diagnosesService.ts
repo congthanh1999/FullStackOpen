9965df6ddcbcb0c0b52d6386a9cda1204eaacda3
congthanh1999 <congthanh10021999@gmail.com>
2024-07-21 13:12:11 +0700

    no message

--- part9/patientor/backend/src/services/diagnosesService.ts
+++
@@ -1,8 +0,0 @@
-import { Diagnosis } from "../types";
-import diagnoses from "../../../data/diagnoses";
-
-const getDiagnoses = (): Diagnosis[] => {
-  return diagnoses;
-};
-
-export default { getDiagnoses };
--- part9/patientor/frontend/src/components/PatientDetails/HospitalEntryDetails.tsx
+++
@@ -1,9 +0,0 @@
-import { HospitalEntry } from "../../types";
-
-const HospitalEntryDetails: React.FC<{ entry: HospitalEntry }> = ({
-  entry,
-}) => {
-  return <div>criteria: {entry.discharge.criteria}</div>;
-};
-
-export default HospitalEntryDetails;
